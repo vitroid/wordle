fc1d4a3169e210fc8a71ea2d82d9afff5caf4271
vitroid <vitroid@gmail.com>
2022-03-17 20:48:50 +0900

    .

---
+++ gh-pages.js
@@ -0,0 +1,16 @@
+var ghpages = require('gh-pages');
+
+ghpages.publish(
+    'public', // path to public directory
+    {
+        branch: 'svelte-static',
+        repo: 'https://github.com/vitroid/wordle.git', // Update to point to your repository  
+        user: {
+            name: 'vitroid', // update to use your name
+            email: 'vitroid@gmail.com' // Update to use your email
+        }
+    },
+    () => {
+        console.log('Deploy Complete!')
+    }
+)
